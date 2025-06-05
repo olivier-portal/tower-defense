f699bc652e14af3c92c66f86212e6010741b6dff
Thibault Caron <thibault.caron@laplateforme.io>
2025-06-05 10:46:48 +0200

    Feat: add doc folder and class diagram

---
+++ cmake-build-debug-ucrt64-msys2/CMakeFiles/tower-defense.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tower-defense.dir/main.cpp.obj"
+  "CMakeFiles/tower-defense.dir/main.cpp.obj.d"
+  "libtower-defense.dll.a"
+  "tower-defense.exe"
+  "tower-defense.exe.manifest"
+  "tower-defense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tower-defense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
